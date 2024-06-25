8566a09b68c418ef2e82ce46156ef45939848588
jesus <jantolin@inhub.es>
2024-06-25 12:03:10 +0200

    Add UserPreference model to handle user supplement preferences

--- gym_recommendation/recomendations/models.py
+++ gym_recommendation/recomendations/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class UserPreference(models.Model):
+    user_id = models.IntegerField(unique=True)
+    protein = models.IntegerField()
+    creatine = models.IntegerField()
+    bcaa = models.IntegerField()
+
+    def __str__(self):
+        return f"User {self.user_id}"
