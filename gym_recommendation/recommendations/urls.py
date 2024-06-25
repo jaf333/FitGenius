11772bf5fef3a48f81002bb77e90895a128e17c6
jesus <jantolin@inhub.es>
2024-06-25 12:33:18 +0200

    Configure URL routing for Wger API integration endpoints

--- gym_recommendation/recommendations/urls.py
+++ gym_recommendation/recommendations/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import RecommendView
+from .views import RecommendView, ExerciseListView, ExerciseDetailView, AuthenticatedExerciseListView
 
 urlpatterns = [
     path('recommend/', RecommendView.as_view(), name='recommend'),
+    path('exercises/', ExerciseListView.as_view(), name='exercises'),
+    path('exercises/<int:exercise_id>/', ExerciseDetailView.as_view(), name='exercise_detail'),
+    path('authenticated_exercises/', AuthenticatedExerciseListView.as_view(), name='authenticated_exercises'),
 ]
