49c228d056f3706ec9ee6dd6b131f64fcd4e7439
jesus <jantolin@inhub.es>
2024-06-25 12:05:50 +0200

    Create RecommendView for handling recommendation requests

--- gym_recommendation/recomendations/views.py
+++ gym_recommendation/recomendations/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .recommender import recommender
 
-# Create your views here.
+class RecommendView(APIView):
+    def get(self, request, format=None):
+        user_id = int(request.query_params.get('user_id'))
+        recommendations = recommender.get_recommendations(user_id)
+        return Response(recommendations, status=status.HTTP_200_OK)
